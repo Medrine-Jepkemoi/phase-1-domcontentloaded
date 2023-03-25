03e84af30fd6718be0c25a67597c185c2c7d5cf2
Medrine-Jepkemoi <jepkemoimedrine@gmail.com>
2023-03-25 17:59:07 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", (e) => {
+    e.preventDefault; 
+    
+    let change = document.getElementById("text")
+    change.textContent = "This is really cool!";
+})
